407aad35d03cdaaeb2e85edc9c4b21d32eef5787
phoebe goldman <a@goldman-tribe.org>
2020-02-20 18:38:54 -0500

    optional type

--- package.lisp
+++ package.lisp
@@ -24,6 +24,7 @@
    :load-and-enter
    :mapf
    :hash-map
+   :optional
    :->
    :|:|
    :skip-nil
--- src/type-definitions.lisp
+++ src/type-definitions.lisp
@@ -3,3 +3,6 @@
 (deftype hash-map (&optional key value)
   (declare (ignore key value))
   'hash-table)
+
+(deftype optional (type)
+  `(or null ,type))
