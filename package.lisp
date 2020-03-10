ea1cd5a6363ff0f4c00d3970c25fd887fecfa1cb
phoebe Goldman <phoebe@pinebookpro>
2020-03-10 13:05:56 -0400

    remove comment with old defpackage form

--- package.lisp
+++ package.lisp
@@ -13,37 +13,3 @@
    :gefjon-utils/type-definitions
    :gefjon-utils/iterate
    :gefjon-utils/adjustable-vector))
-
-;; (defpackage :gefjon-utils
-;;   (:use :cl
-;;         :iterate)
-;;   (:shadow :defstruct
-;;            :defclass)
-;;   (:export
-;;    :slot-descriptor
-;;    :slot-descriptor-type
-;;    :slot-descriptor-name
-;;    :slot-descriptors-types
-;;    :slot-descriptors-names
-;;    :constructor-name
-;;    :defstruct
-;;    :defclass
-;;    :define-boa-constructor-for-class
-;;    :print-all-slots-mixin
-;;    :shallow-copy
-;;    :compiler-state
-;;    :compiler-defun
-;;    :coerce-to-string
-;;    :symbol-concatenate
-;;    :make-keyword
-;;    :check-anaphoric-types
-;;    :load-and-enter
-;;    :mapf
-;;    :hash-map
-;;    :optional
-;;    :->
-;;    :|:|
-;;    :skip-nil
-;;    :slot-name-of
-;;    :adjustable-vector
-;;    :make-adjustable-vector))
