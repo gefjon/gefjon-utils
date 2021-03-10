a2eb3f02293b83d08d6429eb372644f979715f7c
Phoebe Goldman <phoebe@goldman-tribe.org>
2021-03-10 10:10:17 -0500

    refactor existing typedefs, add new ones

--- type-definitions.lisp
+++ type-definitions.lisp
@@ -1,22 +1,29 @@
 (uiop:define-package gefjon-utils/type-definitions
   (:mix cl)
-  (:export hash-map optional func void))
+  (:export
+   function-return-type
+   hash-map optional values! func void tuple list-of form natnum))
 (in-package gefjon-utils/type-definitions)
 
-(defun values-form-p (form)
+(defun form-head-p (form head)
   (and (consp form)
-       (eq (first form) 'values)
-       form))
+       (eq (first form) head)))
 
-(defun void-p (form)
-  (when (eq form 'void)
-    '(values &optional)))
+(deftype values! (&rest types)
+  "Like `cl:values', but implicit additional values are forbidden."
+  (flet ((rest-or-optional-p (thing)
+           (member thing '(&optional &rest) :test #'eq)))
+    (if (find-if #'rest-or-optional-p types) `(values ,@types)
+        `(values ,@types &optional))))
+
+(defun function-return-type (type)
+  (cond ((eq type 'void) '(values!))
+        ((form-head-p type 'values) `(values! ,@(rest type)))
+        ((form-head-p type 'values!) type)
+        (:otherwise `(values! ,type))))
 
 (deftype func (inputs return-type)
-  (let ((values-type (or (values-form-p return-type)
-                         (void-p return-type)
-                         `(values ,return-type &optional))))
-    `(function ,inputs ,values-type)))
+  `(function ,inputs ,(function-return-type return-type)))
 
 (deftype hash-map (&optional key value)
   (declare (ignore key value))
@@ -24,3 +31,21 @@
 
 (deftype optional (type)
   `(or null ,type))
+
+(deftype list-of (&optional element-type)
+  (declare (ignore element-type))
+  'list)
+
+(deftype tuple (&rest elements)
+  (reduce
+   (lambda (new-element existing-type)
+     `(cons ,new-element ,existing-type))
+   elements
+   :from-end t
+   :initial-value 'null))
+
+(deftype form ()
+  t)
+
+(deftype natnum ()
+  '(and fixnum unsigned-byte))
