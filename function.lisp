812f3b3d555f23252c6d98d1739250fb5874af83
Phoebe Goldman <phoebe@goldman-tribe.org>
2021-03-10 10:43:19 -0500

    multiple values support and docstring for ~>

--- function.lisp
+++ function.lisp
@@ -7,17 +7,20 @@
 
 (typedec #'~> (func (function &rest (func (t) t)) (func (&rest t) t)))
 (defun ~> (first-function &rest other-functions)
+  "Left-to-right function compoistion.
+
+Returns a function which invokes FIRST-FUNCTION, then passes its result to the first of the OTHER-FUNCTIONS,
+then passes the new result to the second, and so on."
   (reduce (lambda (accumulator new-function)
-            (declare (type (func (&rest t) t) accumulator)
-                     (type (func (t) t) new-function))
+            (declare (type function accumulator new-function))
             (lambda (&rest args)
-              (funcall new-function (apply accumulator args))))
+              (multiple-value-call new-function (apply accumulator args))))
           other-functions
           :initial-value first-function))
 
 (define-compiler-macro ~> (first-function &rest other-functions)
   `(lambda (&rest args)
      ,(reduce (lambda (accumulator new-function)
-                `(funcall ,new-function ,accumulator))
+                `(multiple-value-call ,new-function ,accumulator))
               other-functions
               :initial-value `(apply ,first-function args))))
