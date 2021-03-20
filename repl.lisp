d32dff4b67b8d8e5d55ae5dbfe0acb4711c8f463
Phoebe Goldman <phoebe@goldman-tribe.org>
2021-03-20 19:33:41 -0400

    func to print numbers as hex

--- repl.lisp
+++ repl.lisp
@@ -1,7 +1,7 @@
 (uiop:define-package gefjon-utils/repl
   (:mix cl)
   (:import-from quicklisp quickload)
-  (:export load-and-enter println))
+  (:export load-and-enter println print-hex))
 (in-package gefjon-utils/repl)
 
 (defmacro load-and-enter (system-package-designator)
@@ -13,3 +13,6 @@
   (fresh-line)
   (apply #'format t format-string stuff)
   (terpri))
+
+(defun print-hex (number)
+  (println "~x" number))
